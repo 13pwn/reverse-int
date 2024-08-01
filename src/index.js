f6b83f4d9b03fc6685e56079cdfb5f1d271922a5
13pwn <mikeoxmaul6z9@gmail.com>
2024-08-02 02:07:09 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  const number = Math.abs(n);
+  for (i = 0; i <= number.toString().length - 1; i++) {
+    result = `${number.toString()[i]}${result}`;
+  }
+  return result;
 }
